2b6f030e50b8f2a9b773c38c60e3dd19e64f4ca7
pratibha hankare <pratibha.bittint@gmail.com>
2018-02-20 11:34:13 +0530

    delete challenges

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -30,11 +30,34 @@ const db=client.db('TodoApp');
 /*
 3.findOneAndDelete->targets one document(row) & returns the same document so that user knows exactly which one deleted.
   Goal: is to delete only one record bt also returning the same record is returning abck so that user knws exactly which record is deleted
-*/
-db.collection('Todos').findOneAndDelete({completed:false}).then(
-  (result)=>{console.log(result);}
-);
 
+  db.collection('Todos').findOneAndDelete({completed:false}).then(
+    (result)=>{console.log(result);}
+  );
+*/
+/****************************************************************************/
+/*challenge :1
+db.collection('Users').deleteMany({name:'abc'}).then(
+  (result)=>{
+    console.log(result);
+  }
+);
+*/
+/*challenge :2
+db.collection('Users').findOneAndDelete({name:'xyz'}).then(
+  (result)=>{
+    console.log(result);
+  }
+);
+*/
+/*challenge :3*/
+db.collection('Users').findOneAndDelete({
+  _id: new ObjectID('5a8bb85fc09b4a060c4df6c9')
+}).then(
+  (result)=>{
+    console.log(result);
+  }
+);
 
 
 
